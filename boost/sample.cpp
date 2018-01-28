f7b83a998649eac825229f9d7f221220eafa8858
taidalab <zdajun1988@163.com>
2018-01-28 13:39:14 +0800

    增加了boost库

--- boost/sample.cpp
+++ boost/sample.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
-#include "boost/lexical_cast.hpp"
+using namespace std;
+using namespace boost::asio;
 
-//-using namespace std;
-
-int main()
+int main(int argc, char* argv[])
 {
-    std::string s="100";
-    int a=boost::lexical_cast<int>(s);
-    int b=1;
-    std::cout<<(a+b) <<std::endl;//输出101
-    return 0;
+        io_service iosev;
+        // 串口COM1, Linux下为“/dev/ttyS0”
+        serial_port sp(iosev, "COM1");
+        // 设置参数
+        sp.set_option(serial_port::baud_rate(19200));
+        sp.set_option(serial_port::flow_control(serial_port::flow_control::none));
+        sp.set_option(serial_port::parity(serial_port::parity::none));
+        sp.set_option(serial_port::stop_bits(serial_port::stop_bits::one));
+        sp.set_option(serial_port::character_size(8));
+        // 向串口写数据
+        write(sp, buffer("Hello world", 12));
+
+        // 向串口读数据
+        char buf[100];
+        read(sp, buffer(buf));
+
+        iosev.run();
+        return 0;
 }
